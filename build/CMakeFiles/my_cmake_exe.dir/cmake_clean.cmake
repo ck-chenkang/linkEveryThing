0b496aa7a440b89b2068dcbc99e7becfcaf29779
ck-chenkang <bettermanchenkang@163.com>
2021-03-19 14:50:47 +0800

    init project

---
+++ build/CMakeFiles/my_cmake_exe.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_cmake_exe.dir/main.cpp.o"
+  "my_cmake_exe.pdb"
+  "my_cmake_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_cmake_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    cout << "Program is begnning!!";
+
+    return 0;
+}
